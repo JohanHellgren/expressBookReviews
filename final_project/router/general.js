8469487c8b236d2559cb01031c748120baf02bc2
JohanHellgren <johanhellgren@protonmail.com>
2025-08-19 10:06:48 -0400

    fin d by auythor

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -45,14 +45,31 @@ public_users.get('/isbn/:isbn', function (req, res) {
   });  
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author.toLowerCase();
   
-});
+    const foundBooks = [];
+  
+    Object.keys(books).forEach(key => {
+      const book = books[key];
+      if (book.author.toLowerCase() === author) {
+        foundBooks.push({ isbn: key, ...book });
+      }
+    });
+  
+    if (foundBooks.length > 0) {
+      return res.status(200).json({
+        message: `Books by ${author}:`,
+        books: foundBooks
+      });
+    } else {
+      return res.status(404).json({ message: "No books found by author" });
+    }
+  });  
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
 });
 
 //  Get book review
