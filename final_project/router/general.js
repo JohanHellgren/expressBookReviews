5e1ea495a43688ad14c825bd8ba34b5719464635
JohanHellgren <johanhellgren@protonmail.com>
2025-08-19 04:22:37 -0400

    init

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,8 +12,13 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  new Promise((resolve, reject) => {
+    resolve(books)
+  }).then(data => {
+    return res.status(200).json(data)
+  }).then (err => {
+    return res.status(500).json(err)
+  })
 });
 
 // Get book details based on ISBN
