ff046ff17a934ea54facc59d9b55b9107fdd9950
JohanHellgren <johanhellgren@protonmail.com>
2025-08-21 06:08:45 -0400

    add remove rewre

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -34,9 +34,38 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn
+    const review = req.query.review
+    const username = req.session.authorization?.username
+
+    if(!username) {
+        return res.status(401).json({message: "User not logged in"})
+    }
+
+    if(!books[isbn]) {
+        return res.status(200).json({message: "Book not found"})
+    }
+
+    books[isbn].reviews[username] = review
+    return res.status(200).json({message: "Review added!"})
 });
+
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.session.authorization?.username;
+  
+    if (!username) {
+      return res.status(401).json({ message: "User not logged in" });
+    }
+  
+    if (!books[isbn] || !books[isbn].reviews[username]) {
+      return res.status(404).json({ message: "Review not found for this user" });
+    }
+  
+    delete books[isbn].reviews[username];
+    return res.status(200).json({ message: "Review deleted successfully" });
+  });
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
